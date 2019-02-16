f3018d93afd652bc3d00b6a3585012d186a4e308
jae kaplan <me@jkap.io>
2019-02-15 21:30:57 -0800

    Updates

---
+++ precache-manifest.e3c908ee22ef4d8ef24e493a87057c1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cab7f7b12dc96e1a57cd",
+    "url": "/owl-badge-generator/static/css/main.c3a1173f.chunk.css"
+  },
+  {
+    "revision": "cab7f7b12dc96e1a57cd",
+    "url": "/owl-badge-generator/static/js/main.cab7f7b1.chunk.js"
+  },
+  {
+    "revision": "aa30a758055c59234971",
+    "url": "/owl-badge-generator/static/js/runtime~main.aa30a758.js"
+  },
+  {
+    "revision": "f645bcb2c763188d0075",
+    "url": "/owl-badge-generator/static/js/2.f645bcb2.chunk.js"
+  },
+  {
+    "revision": "c3655ec76d9e73a2da2c5540c79ee846",
+    "url": "/owl-badge-generator/index.html"
+  }
+];
